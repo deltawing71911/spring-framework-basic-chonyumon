7890cbb799657f849e4b1beeaf6d3c38baaf1561
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2023-11-15 23:03:27 +0900

    2023-11-15 DISample

---
+++ DependencyInjectionSample/src/main/java/com/example/demo/chapter03/used/MorningGreet.java
@@ -0,0 +1,15 @@
+package com.example.demo.chapter03.used;
+
+import org.springframework.stereotype.Component;
+
+//@Component
+public class MorningGreet implements Greet {
+
+    @Override
+    public void greeting() {
+        System.out.println("-------------------");
+        System.out.println("Good Morning!");
+        System.out.println("-------------------");
+    }
+
+}
