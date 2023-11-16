13b2460a57dbab04b805fd02b5ad53941bf69913
Suji Ahn <137012201+deltawing71911@users.noreply.github.com>
2023-11-16 12:24:47 +0900

    2023-11-16 Spring MVC Model Sample

---
+++ SpringMVCModelSample/src/test/java/com/example/springmvcmodelsample/SpringMvcModelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcmodelsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcModelSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
